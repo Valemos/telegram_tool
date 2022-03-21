9586d30aca50e70c98bff06cfbb8cc08b9af79aa
Anton Skrypnyk <programworkerbox@gmail.com>
2022-03-21 13:12:23 +0200

    Refactor code. Move app configurations to environment variables.

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 from pathlib import Path
 
@@ -7,8 +8,10 @@ api_id = os.environ["APP_ID"]
 api_hash = os.environ["APP_HASH"]
 # ---------------------------------------
 
-path = Path("__file__").parent / "saved_session.session"
+session_path = str((Path(__file__).parent / "saved_session.session").absolute())
 
-client = TelegramClient(path, api_id, api_hash)
-client.connect()
-client.start()
+if __name__ == '__main__':
+    client = TelegramClient(session_path, api_id, api_hash)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(client.connect())
+    client.start()
